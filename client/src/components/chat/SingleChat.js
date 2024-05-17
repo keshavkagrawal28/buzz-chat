c2caf09e200648405cc2e7a924ea630bc6e859b6
keshavkagrawal28@gmail.com <keshav.agrawal@eka1.com>
2024-05-17 09:04:31 +0530

    disconnect socket when component unmounts

--- client/src/components/chat/SingleChat.js
+++ client/src/components/chat/SingleChat.js
@@ -105,6 +105,10 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
     socket.on('stop typing', () => {
       setIsTyping(false);
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   useEffect(() => {
